270fb31d08441184783ae52ffbadc8f286c3e7ba
Nhat <hoang.nhat@littlelives.com.vn>
2021-04-28 09:08:13 +0700

    Init project

---
+++ iOSTemplate/iOSTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTemplate
+//
+//  Created by Nhat on 28/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
